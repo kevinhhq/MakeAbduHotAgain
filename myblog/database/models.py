6d989afbe2fd1545308f6c2833ef5de3f92a8a40
lmiao4 <lmiao4@illinois.edu>
2019-03-28 03:55:17 -0500

    [feature] Backend: Initialize table models. Migration incomplete.

--- myblog/database/models.py
+++ myblog/database/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class food_table(models.Model):
@@ -12,5 +13,64 @@ class food_table(models.Model):
 
     def __str__(self):
         return self.Name
+
+
+class DietPlan(models.Model):
+    name = models.CharField(max_length=20) # diet plan name 
+    date = models.DateTimeField(default=datetime.now, blank=True)
+    user = models.ForeignKey(
+        'User',
+        on_delete=models.CASCADE
+        )
+    goal = models.CharField() # goal of plan: loss weight, gain weight
+    status = models.BooleanField(default=False) # false = unfinished
+    carb = models.IntegerField()
+    fiber = models.IntegerField()
+    protein = models.IntegerField()
+    fat = models.IntegerField()
+    # optional??
+    foods = madels.ManyToManyField(
+        food_table, 
+        through='GeneratedBy', 
+        through_fields=('planID', 'foodID'))
+
+    def __str__(self):
+        return self.name
+
+class GeneratedBy(models.Model):
+    planID = models.ForeignKey(
+        DeitPlan, 
+        on_delete=models.CASCADE)
+    foodID = models.ForeignKey(
+        food_table, 
+        on_delete=models.CASCADE)
+    amount = models.IntegerField()
+
+# TODO: extend rest-auth user model. NOT finished. 
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+    # custom fields for user
+    gender = models.CharField(default='undefined')
+    height = models.IntegerField()
+    age = models.IntegerField()
+    cur_weight = models.IntegerField()
+    tgt_weight = models.IntegerField()
+    is_cook = models.BooleanField()
+    plan = models.IntegerField()
+    total_cal = models.IntegerField()
+    carb = models.IntegerField()
+    fib = models.IntegerField()
+    protein = models.IntegerField()
+    fat = models.IntegerField()
+
+    def __str__(self):
+        return self.user
+
+
+# TODO: Not sure how to set up this table
+'''class ChartInfo(models.Model):
+    date = models.DateTimeField(default=datetime.now, blank=True)
+    username = models.CharField() 
+    weight = models.IntegerField()'''
 
 
